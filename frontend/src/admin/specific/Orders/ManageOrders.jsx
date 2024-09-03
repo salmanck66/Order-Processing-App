735f8df41f87dce61bc562be5c4b980309fdf1d6
fahiz <fahizk100@gmail.com>
2024-09-03 16:44:44 +0530

    admin manage orders

--- frontend/src/admin/specific/Orders/ManageOrders.jsx
+++ frontend/src/admin/specific/Orders/ManageOrders.jsx
@@ -1,26 +1,11 @@
 import React from 'react';
-import { Table, Card } from 'antd';
+import { Card, message } from 'antd';
+import CustomerCard from './CustomerCard';
 
 const ManageOrders = ({ orders }) => {
   // Define columns for the orders table
-  const columns = [
-    {
-      title: 'Product ID',
-      dataIndex: 'productId',
-      key: 'productId',
-    },
-    {
-      title: 'Sizes & Quantities',
-      dataIndex: 'orderSizes',
-      key: 'orderSizes',
-      render: (sizes) =>
-        Object.entries(sizes).map(([size, quantity]) => (
-          <div key={size}>
-            {size}: {quantity}
-          </div>
-        )),
-    },
-  ];
+ 
+
 
   return (
     <div style={{ padding: '20px' }}>
@@ -31,23 +16,10 @@ const ManageOrders = ({ orders }) => {
           style={{ marginBottom: '20px' }}
         >
           {resellerOrder.customers.map((customer) => (
-            <Card
-              key={customer._id}
-              title={`Customer: ${customer.customerName}`}
-              style={{ marginBottom: '20px' }}
-            >
-              {customer.orders.length > 0 ? (
-                <Table
-                  dataSource={customer.orders}
-                  columns={columns}
-                  rowKey="_id"
-                  pagination={false}
-                />
-              ) : (
-                <p>No orders available</p>
-              )}
-              <p>Label: {customer.label}</p>
-            </Card>
+            <CustomerCard 
+              key={customer._id} 
+              customer={customer} 
+            />
           ))}
         </Card>
       ))}
