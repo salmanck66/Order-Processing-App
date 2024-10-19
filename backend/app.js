92ab91c866c9eb7032fc66e411b8b89192a2764a
fahiz <fahizk100@gmail.com>
2024-10-19 15:17:46 +0530

    make chane server

--- backend/app.js
+++ backend/app.js
@@ -10,24 +10,38 @@ import bodyParser from 'body-parser';
 
 
 const app = express();
-// app.use(fileUpload({
-//   createParentPath: true 
-// }));
+
 
 app.use(fileUpload());
+console.log(process.env.FRONTEND_URL);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    
-  }));
+  origin: 'https://order-processing-app-tffa.vercel.app',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+}));
+
+  
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      res.header('Access-Control-Allow-Credentials', 'true');
+      return res.status(204).end(); 
+  }
+  next();
+});
+
+  
 app.use(express.json())
 app.use('/user', userrouter);
 app.use('/admin', adminroutes);
 connectDB()
 app.listen(3000, () => {
     console.log("Server Is Ready");
+
 });
